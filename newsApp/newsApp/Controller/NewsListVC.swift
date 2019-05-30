a5963a2338084b36256286b7fc618424d9d58adc
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-30 14:38:54 +0800

    configure newsListTableCell in NewsListVC

--- newsApp/newsApp/Controller/NewsListVC.swift
+++ newsApp/newsApp/Controller/NewsListVC.swift
@@ -14,9 +14,23 @@ class NewsListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
     }
 
 
 }
 
+extension NewsListVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
